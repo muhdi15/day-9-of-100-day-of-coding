810accbf673f13dc2e45ce7c6c4a8786e157a23b
muhdi15 <115169961+muhdi15@users.noreply.github.com>
2022-10-20 10:12:41 +0800

    Add files via upload

---
+++ code 9.py
@@ -0,0 +1,8 @@
+a = int(input("masukkan tinggi anda:"))
+
+if(a < 160):
+    print("tinggi anda belum mencukupi")
+elif(a>=160 and a<=180):
+    print("tinggi anda sudah mencukupi")
+else:
+    print("tinggi anda melebihi standar")
